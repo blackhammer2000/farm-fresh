11354ef9489f7d8ecb0e03932003e47c89a0b51e
wawerusamuel <waweruzamuel@gmail.com>
2023-04-08 17:36:24 +0300

    removed unwanted comments...

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -7,13 +7,3 @@ import About from "../components/About.vue";
     <About />
   </main>
 </template>
-
-<!-- <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style> -->

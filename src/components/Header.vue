2d27fa22a658e9d20c32a6deaf9ac2f018063ac8
wawerusamuel <waweruzamuel@gmail.com>
2023-04-08 16:58:27 +0300

    first commit...

---
+++ src/components/Header.vue
@@ -0,0 +1,104 @@
+<script setup>
+ import { RouterLink } from "vue-router";
+</script>
+
+<template>
+    <header>
+      <!-- <div class="wrapper">
+        <h1>+0123456789</h1>
+      </div>
+  
+      <div class="wrapper">
+        <h1>FarmFresh</h1>
+      </div>
+  
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/students">Students</RouterLink>
+      </nav> -->
+
+
+    <!-- Topbar Start -->
+    <div class="container-fluid px-5 d-none d-lg-block">
+        <div class="row gx-5 py-3 align-items-center">
+            <div class="col-lg-3">
+                <div class="d-flex align-items-center justify-content-start">
+                    <i class="bi bi-phone-vibrate fs-1 text-primary me-2"></i>
+                    <h2 class="mb-0">+012 345 6789</h2>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="d-flex align-items-center justify-content-center">
+                    <a href="index.html" class="navbar-brand ms-lg-5">
+                        <h1 class="m-0 display-4 text-primary"><span class="text-secondary">Farm</span>Fresh</h1>
+                    </a>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="d-flex align-items-center justify-content-end">
+                    <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-twitter">a</i></a>
+                    <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-facebook-f">b</i></a>
+                    <a class="btn btn-primary btn-square rounded-circle me-2" href="#"><i class="fab fa-linkedin-in">c</i></a>
+                    <a class="btn btn-primary btn-square rounded-circle" href="#"><i class="fab fa-instagram">d</i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-lg bg-primary navbar-dark shadow-sm py-3 py-lg-0 px-3 px-lg-5">
+        <a href="index.html" class="navbar-brand d-flex d-lg-none">
+            <h1 class="m-0 display-4 text-secondary"><span class="text-white">Farm</span>Fresh</h1>
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav mx-auto py-0">
+                <!-- <a href="index.html" class="nav-item nav-link active">Home</a> -->
+                <RouterLink class="nav-item nav-link active" to="/">Home</RouterLink>
+
+                <!-- <a href="about.html" class="nav-item nav-link">About</a> -->
+                <RouterLink class="nav-item nav-link " to="/about">About</RouterLink>
+
+
+                <!-- <a href="service.html" class="nav-item nav-link">Service</a> -->
+                <RouterLink class="nav-item nav-link " to="/services">Services</RouterLink>
+
+                
+                <!-- <a href="product.html" class="nav-item nav-link">Product</a> -->
+                <RouterLink class="nav-item nav-link " to="/product">Product</RouterLink>
+
+                <RouterLink class="nav-item nav-link " to="/pages">Pages</RouterLink>
+
+
+                <!-- <div class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                    <div class="dropdown-menu m-0">
+                        <a href="blog.html" class="dropdown-item">Blog Grid</a>
+                        <a href="detail.html" class="dropdown-item">Blog Detail</a>
+                        <a href="feature.html" class="dropdown-item">Features</a>
+                        <a href="team.html" class="dropdown-item">The Team</a>
+                        <a href="testimonial.html" class="dropdown-item">Testimonial</a>
+                    </div>
+                </div> -->
+
+                <!-- <a href="contact.html" class="nav-item nav-link">Contact</a> -->
+                <RouterLink class="nav-item nav-link " to="/contact">Contact</RouterLink>
+
+
+            </div>
+        </div>
+    </nav>
+</header>
+</template>
+
+
+
+
+
+
+  
---
+++ src/components/StudentsComponent.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Students Data</h1>
+  <h1>{{ studetsStore }}</h1>
+  <h1>Students Data</h1>
+</template>
+
+<script setup>
+let studentsStore = useStudentStore();
+</script>

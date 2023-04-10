53c5d017721ba5fede69f3a6ef930214ab909343
wawerusamuel <waweruzamuel@gmail.com>
2023-04-10 13:24:07 +0300

    working on adding crud operations...

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -87,15 +87,15 @@ export default {
             <h6 class="mb-3">{{ product.name }}</h6>
             <h5 class="text-primary mb-0">{{ product.price }}</h5>
             <div class="btn-action d-flex justify-content-center">
-              <a
+              <button
                 @click="() => deleteProduct(product.id)"
                 class="btn bg-primary py-2 px-3"
-                href=""
-                ><i class="bi bi-cart text-white"> Delete</i></a
               >
-              <a class="btn bg-secondary py-2 px-3" href=""
-                ><i class="bi bi-eye text-white">Edit</i></a
-              >
+                <i class="bi bi-cart text-white"> Delete</i>
+              </button>
+              <button class="btn bg-secondary py-2 px-3">
+                <i class="bi bi-eye text-white">Edit</i>
+              </button>
             </div>
           </div>
         </div>

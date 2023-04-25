99af5c60e283081f935fbff51afa41cc6fe90c49
wawerusamuel <waweruzamuel@gmail.com>
2023-04-25 09:35:51 +0300

    added the epxress package...

---
+++ server/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const { connect } = require("mongoose");
+
+const cors = require("cors");
+
+const PORT = process.env.PORT || 8081;
+
+const routes = require("./routes/routes");
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5501" || "http://127.0.0.1:5501",
+  })
+);
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}.`);
+});
+
+connect(process.env.DB_CONNECTION, () => {
+  console.log("connected to database.");
+});
